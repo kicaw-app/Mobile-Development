16144b9a8a7273e3c80c1b784a81c17574c9eb49
WildanFrananda <wildanrailfanskrl@gmail.com>
2023-12-21 18:53:42 +0700

    feat: Add forum preview

--- app/src/main/java/com/satriopndt/kicawcapstone/ui/forum/ForumScreen.kt
+++ app/src/main/java/com/satriopndt/kicawcapstone/ui/forum/ForumScreen.kt
@@ -19,15 +19,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
 import com.satriopndt.kicawcapstone.R
 import com.satriopndt.kicawcapstone.ViewModelFactory
 import com.satriopndt.kicawcapstone.di.Injection
 import com.satriopndt.kicawcapstone.navigation.Screen
 import com.satriopndt.kicawcapstone.ui.component.ForumCard
 import com.satriopndt.kicawcapstone.ui.component.SearchBar
+import com.satriopndt.kicawcapstone.ui.home.HomeScreen
+import com.satriopndt.kicawcapstone.ui.theme.KicawCapstoneTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -107,3 +111,11 @@ fun ForumScreen(
 
 // Define a data class to hold the chat information
 data class Chat(val title: String, val message: String, val time: String)
+
+@Preview(showBackground = true)
+@Composable
+fun ForumScreenPreview() {
+    KicawCapstoneTheme {
+        ForumScreen(navController = rememberNavController())
+    }
+}
